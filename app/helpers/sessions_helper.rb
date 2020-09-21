b17bf49be3d95ddaaa3007339afc529b0701e0e8
GEphrau <ephraubi@gmail.com>
2020-09-21 13:13:04 +0100

    Fix every failed test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,29 +1,28 @@
 module SessionsHelper
-    
-    def log_in(user)
-        session[:user_id] = user.id
-      end
+  
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-     # Returns the current logged-in user (if any).
-     def current_user
-        if (user_id = session[:user_id])
-          @current_user ||= User.find_by(id: user_id)
-        elsif (user_id = cookies.encrypted[:user_id])
-          user = User.find_by(id: user_id)
-          if user && user.authenticated?(cookies[:remember_token])
-            log_in user
-            @current_user = user
-          end
-        end
+  # Returns the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
-
+    end
+  end
 
   def logged_in?
     !current_user.nil?
   end
 
-   # Logs out the current user.
-   def log_out
+  # Logs out the current user.
+  def log_out
     reset_session
     @current_user = nil
   end
